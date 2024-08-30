4d6e42b1ea752394919c663b7d52f3a7b489f455
Omonuwa Desmond Osaigbovo <omonuwaosai@gmail.com>
2024-08-30 08:10:25 +0000

    calculate surplus data

--- run.py
+++ run.py
@@ -78,7 +78,13 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()  
     stock_row =stock[-1]
-    print(stock_row)  
+     
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data 
 
 def main():
     """
@@ -86,7 +92,8 @@ def main():
     """
     sales_data = get_sales_data()  # Get sales data from user input
     update_sales_worksheet(sales_data)  # Update the sales worksheet
-    calculate_surplus_data(sales_data)  # Currently, this function only prints stock data
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)  # Currently, this function only prints stock data
 
 print("Welcome to Love Sandwiches Automation")
 main()
